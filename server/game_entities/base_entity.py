82720479335f4123b4d0e2a686c9c6ce9baf7990
FullJoin.io <>
2019-10-16 23:47:37 +0100

    Add pymunk as a dependency. Add base entity class and amended player class to inherit from it.

---
+++ server/game_entities/base_entity.py
@@ -0,0 +1,8 @@
+import server.utils.helper as helper
+
+class BaseEntity:
+
+    def __init__(self):
+
+        self.id = helper.get_uuid()
+        self.pos = {'x': None, 'y': None}
--- server/game_entities/player.py
+++ server/game_entities/player.py
@@ -1,6 +1,24 @@
-class Player:
+from server.game_entities.base_entity import BaseEntity
+import pymunk
 
-    def __init__(self, client):
-        self.id = client.id
-        self.client = client
-        self.pos = {'x': 0, 'y': 0}
+class Player (BaseEntity):
+
+    def __init__(self, client_id):
+
+        super().__init__()
+
+        # Replace base auto generated id with client id
+        self.id = client_id
+
+        self.mass = 10
+        self.radius = 25
+        self.inertia = pymunk.moment_for_circle(self.mass, 0, self.radius, (0, 0))
+        
+        self.body = pymunk.Body(self.mass, self.inertia)
+        self.body.position = 0, 0
+        
+        self.shape = pymunk.Circle(self.body, self.radius, (0, 0))
+        self.shape.elasticity = 0.95
+        self.shape.friction = 0.9
+        
+        
